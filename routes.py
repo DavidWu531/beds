6dded7cb86e4b010c1c3b21729597e92207e341c
Nosweat420 <davidwu93447@gmail.com>
2024-08-16 13:28:17 +1200

    Separated all database routes

--- routes.py
+++ routes.py
@@ -16,12 +16,14 @@ def not_found(e):
     return render_template('404.html'), 404
 
 
+# Get combo id submitted by user
 @app.route('/cid_submit', methods=['POST'])  # Gets ComboId from form
 def get_combo_id():
     id = request.form.get('cid')
     return redirect("/combos/" + str(id))
 
 
+# Account login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     get_flashed_messages()
@@ -49,8 +51,10 @@ def login():
         return render_template('login.html')
 
 
+# Account register route
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    get_flashed_messages()
     if 'username' in session:
         return redirect("/dashboard")
     else:
@@ -82,8 +86,10 @@ def register():
         return render_template('register.html')
 
 
+# Account dashboard route
 @app.route('/dashboard')
 def dashboard():
+    get_flashed_messages()
     if 'username' in session:
         return render_template('dashboard.html')
     else:
@@ -91,15 +97,19 @@ def dashboard():
         return redirect("/login")
 
 
+# Account logout route
 @app.route('/logout')
 def logout():
+    get_flashed_messages()
     session.pop('username', None)
     flash('You have successfully logged out.')
     return redirect("/")
 
 
+# Account delete route
 @app.route('/delete')
 def delete():
+    get_flashed_messages()
     username = session['username']
 
     conn = sqlite3.connect('account.db')
@@ -112,89 +122,109 @@ def delete():
     return redirect("/")
 
 
-# All routes respective to its table except many-to-many relationship table
-@app.route('/<string:data>/', defaults={'id': None})
+# Bed Base Route
+@app.route('/bed_base/', defaults={"id": None})
+# Get all or one data from bed base table depending on id
+@app.route('/bed_base/<int:id>')
 # Ensures that id can be empty as well to allow redirecting
-@app.route('/<string:data>/<int:id>')
-def routes(data, id):
+def bed_base(id):
     conn = sqlite3.connect('beds.db')
     cur = conn.cursor()
-    if data == "bed_base":
-        # Checks if data is bed base and checks for id
-        base = None
-        if id is None:
-            # Checks if id is empty and redirects to id=0
-            return redirect("/bed_base/0")
-        elif id == 0:
-            # Grabs everything if id is 0
-            # Else grab individual data
-            cur.execute('SELECT * FROM Base')
-            base = cur.fetchall()
-            return render_template('all_base.html', base=base)
-        else:
-            cur.execute('SELECT * FROM Base WHERE BaseID=?', (id,))
-            base = cur.fetchone()
-            if base is None:
-                # Returns 404 error page when nothing is found
-                # Else returns available data
-                return render_template('404.html'), 404
-            else:
-                return render_template('individual_base.html', base=base)
-    elif data == "mattress":
-        # Else if data is mattress and checks for id
-        mattress = None
-        if id is None:
-            # Checks if id is empty and redirects to id=0
-            return redirect("/mattress/0")
-        elif id == 0:
-            cur.execute('SELECT * FROM Mattress')
-            mattress = cur.fetchall()
-            return render_template('all_mattress.html', mattress=mattress)
-        else:
-            cur.execute('SELECT * FROM Mattress WHERE MattressID=?', (id,))
-            mattress = cur.fetchone()
-            if mattress is None:
-                # Returns 404 error page when nothing is found
-                # Else returns available data
-                return render_template('404.html'), 404
-            else:
-                return render_template('individual_mattress.html', mattress=mattress)
-    elif data == "blanket":
-        # Else if data is blanket and checks for id
-        blanket = None
-        if id is None:
-            # Checks if id is empty and redirects to id=0
-            return redirect("/blanket/0")
-        elif id == 0:
-            cur.execute('SELECT * FROM Blankets')
-            blanket = cur.fetchall()
-            return render_template('all_blanket.html', blanket=blanket)
-        else:
-            cur.execute('SELECT * FROM Blankets WHERE BlanketID=?', (id,))
-            blanket = cur.fetchone()
-            if blanket is None:
-                # Returns 404 error page when nothing is found
-                # Else returns available data
-                return render_template('404.html'), 404
-            else:
-                return render_template('individual_blanket.html', blanket=blanket)
+    base = None
+    if id is None:
+        # Checks if id is empty and redirects to id=0
+        return redirect("/bed_base/0")
+    elif id == 0:
+        # Grabs everything if id is 0
+        # Else grab individual data
+        cur.execute('SELECT * FROM Base')
+        base = cur.fetchall()
+        return render_template('all_base.html', base=base)
     else:
-        # Data doesn't match above conditions so returns 404 error page
-        return render_template('404.html'), 404
+        cur.execute('SELECT * FROM Base WHERE BaseID=?', (id,))
+        base = cur.fetchone()
+        if base is None:
+            # Returns 404 error page when nothing is found
+            # Else returns available data
+            return render_template('404.html'), 404
+        else:
+            return render_template('individual_base.html', base=base)
+
+
+# Mattress Route
+@app.route('/mattress/', defaults={"id": None})
+# Get all or one data from mattress table depending on id
+@app.route('/mattress/<int:id>')
+# Ensures that id can be empty as well to allow redirecting
+def mattress(id):
+    conn = sqlite3.connect('beds.db')
+    cur = conn.cursor()
+    mattress = None
+    if id is None:
+        # Checks if id is empty and redirects to id=0
+        return redirect("/mattress/0")
+    elif id == 0:
+        # Grabs everything if id is 0
+        # Else grab individual data
+        cur.execute('SELECT * FROM Mattress')
+        mattress = cur.fetchall()
+        return render_template('all_mattress.html', mattress=mattress)
+    else:
+        cur.execute('SELECT * FROM Mattress WHERE MattressID=?', (id,))
+        mattress = cur.fetchone()
+        if mattress is None:
+            # Returns 404 error page when nothing is found
+            # Else returns available data
+            return render_template('404.html'), 404
+        else:
+            return render_template('individual_mattress.html', mattress=mattress)
+
+
+@app.route('/blanket/', defaults={"id": None})
+# Get all or one data from blanket table depending on id
+@app.route('/blanket/<int:id>')
+# Ensures that id can be empty as well to allow redirecting
+def routes(id):
+    conn = sqlite3.connect('beds.db')
+    cur = conn.cursor()
+    blanket = None
+    if id is None:
+        # Checks if id is empty and redirects to id=0
+        return redirect("/blanket/0")
+    elif id == 0:
+        # Grabs everything if id is 0
+        # Else grab individual data
+        cur.execute('SELECT * FROM Blankets')
+        blanket = cur.fetchall()
+        return render_template('all_blanket.html', blanket=blanket)
+    else:
+        cur.execute('SELECT * FROM Blankets WHERE BlanketID=?', (id,))
+        blanket = cur.fetchone()
+        if blanket is None:
+            # Returns 404 error page when nothing is found
+            # Else returns available data
+            return render_template('404.html'), 404
+        else:
+            return render_template('individual_blanket.html', blanket=blanket)
 
 
 # Many-to-many relationship route
 @app.route("/combos/", defaults={'id': None})
+# Get all or one data from many-to-many relationship table depending on id
 @app.route("/combos/<int:id>")
+# Ensures that id can be empty as well to allow redirecting
 def combos(id):
     conn = sqlite3.connect('beds.db')
     cur = conn.cursor()
     combo = None
     if id is None:
+        # Checks if id is empty and redirects to id=0
         return redirect("/combos/0")
     elif id == 0:
+        # Renders combo input template for inputting combo id
         return render_template('combo_input.html', combo=combo)
     else:
+        # Get combo based on id
         cur.execute('SELECT BedCombos.RelationshipID,\
                             Base.*, \
                             Blankets.*, \
--- static/node.js
+++ static/node.js
@@ -1,20 +1,15 @@
-console.log("Hello, World!");
-console.log("f");
+function show_password() {
+    var x = document.getElementById("password");
+    var y = document.getElementById("confirm-password");
+    if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
 
-// ABANDONED FOR NOW
-
-// function show_password() {
-//     var x = document.getElementById("password");
-//     var y = document.getElementById("confirm-password");
-//     if (x.type === "password") {
-//     x.type = "text";
-//   } else {
-//     x.type = "password";
-//   }
-
-//     if (y.type === "password") {
-//       y.type = "text";
-//     } else {
-//       y.type = "password"
-//     }
-//     }
+    if (y.type === "password") {
+      y.type = "text";
+    } else {
+      y.type = "password"
+    }
+    }
